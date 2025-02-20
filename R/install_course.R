205feacd7b2138f08de0772d2e0a23b79af235ec
jcarmezim <jcarmezim@igtp.cat>
2025-02-20 12:59:24 +0100

    Install course function

---
+++ R/install_course.R
@@ -0,0 +1,24 @@
+#' Install workshop materials
+#'
+#' `install_course()` will install the workshop materials for the workshop
+#' Curs aplicat de bioestadística amb R. Then, it will open a new RStudio
+#' Project containing the files you'll need.
+#'
+#' @param destdir The path on your computer where you would like the workshop
+#'   installed. By default, this will install somewhere conspicuous, like your
+#'   desktop, although you can tell `install_course()` exactly where you want
+#'   it to download.
+#'
+#' @export
+
+install_course <- function(destdir = getOption("usethis.destdir")) {
+
+  usethis::use_course(
+
+    "bruigtp/curs_bioestadistica_R",
+
+    destdir = destdir
+
+  )
+
+}

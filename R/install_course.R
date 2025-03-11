9beafda0d91c6ebe6c829e6a24503ed9bb90e666
psatorra <pau.satorra@gmail.com>
2025-03-11 12:30:42 +0100

    create directory

--- R/install_course.R
+++ R/install_course.R
@@ -1,6 +1,6 @@
 #' Install workshop materials
 #'
-#' `install_course()` will install the workshop materials for the workshop
+#' `install_course()` will install the workshop materials for the workshop in a new repository.
 #' Curs aplicat de bioestadística amb R. Then, it will open a new RStudio
 #' Project containing the files you'll need.
 #'
@@ -9,20 +9,30 @@
 #'   desktop, although you can tell `install_course()` exactly where you want
 #'   it to download.
 #' @param session Integer belonging to the session number that you want to install.
+#' @param overwrite logical indicating if the directory has to be overwritten or not.
 #'
 #' @export
 
-install_course <- function(destdir = getOption("usethis.destdir"), session = NULL) {
+install_course <- function(destdir = getOption("usethis.destdir"), session = NULL, overwrite = FALSE) {
 
   if(is.null(session)) {
     stop("A session number has to be specified")
   }
 
+  #Create directory
+  dir <- paste0(destdir, "/Session", session)
+
+  if(dir.exists(dir) & !overwrite) {
+    stop(paste0("This directory already exists: '", dir, "'. If you want to overwrite it set `overwrite = TRUE`."))
+  }
+
+  dir.create(dir)
+
   usethis::use_course(
 
     "bruigtp/curs_bioestadistica_R",
 
-    destdir = paste0(destdir, "/Session", session)
+    destdir = dir
 
   )
 
